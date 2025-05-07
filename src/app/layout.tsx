429e7af2e2ef38f73adbf0ea230bf8f05e35da60
eduardo-afonso24 <eduardo.afonso@netag.ao>
2025-05-07 11:41:51 +0100

    add google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Provider from '@/contexts/ContextProvider'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Provider from "@/contexts/ContextProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'PN Clique',
+  title: "PN Clique",
   description:
-    'Somos uma empresa de tecnologia especializada no desenvolvimento de soluções móveis e web, que impactam positivamente a vida de centenas de pessoas e empresas.',
+    "Somos uma empresa de tecnologia especializada no desenvolvimento de soluções móveis e web, que impactam positivamente a vida de centenas de pessoas e empresas.",
   icons: {
-    icon: '/favicon.ico',
+    icon: "/favicon.ico",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-PT">
@@ -25,12 +25,12 @@ export default function RootLayout({
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <meta
           name="google-site-verification"
-          content="Vsx1PTIBGdl2s7Bt4oGCIDBbtbIRorCnJtyHD__8DJg"
+          content="kp5CFdCghKB3AeF5lKLFhnp-yb1TzHVhMI8LuvRs-7I"
         />
       </head>
       <body className={inter.className}>
         <Provider>{children}</Provider>
       </body>
     </html>
-  )
+  );
 }
